ed218da4dda12b601821fd89e78d9d7ab162e5d8
remysedlak <remysedlak@gmail.com>
2026-03-23 12:28:50 -0400

    add icon

--- docs/conf.py
+++ docs/conf.py
@@ -11,6 +11,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "breeze"
 html_title = "Remy Sedlak"
 html_static_path = ["_static"]
+html_favicon = "_static/images/ic.png"
 html_css_files = ["custom.css"]
 
 html_theme_options = {}
